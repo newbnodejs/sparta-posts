efa866775fea189238adac0d0765301142f52538
newbnodejs <newbnodejs@gmail.com>
2023-01-30 15:59:49 +0900

    API 작성 추가

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,32 @@ const express = require("express");
 const router = express.Router();
 const Post = require("./schema/post");
 
-router.get('/', async (_, res) => {
+router.get("/", async (_, res) => {
     try {
         const posts = await Post.find(); // [{}, {}, {}]
         res.json({ data: posts });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post("/", async (req, res) => {
+    const { user, title, content, password} = req.body;
+
+    // 객체 구조
+    // const user = req.body.user;
+    // const title = req.body.title;
+    // const content = req.body.content;
+    // const password = req.body.password;
+    
+    try {
+        const post = await Post.create({
+            user,
+            title,
+            content,
+            password
+        });
+        res.json({ data: post });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
