d383a79c2f90993f6b1fc06799c93e0e4769ff17
newbnodejs <newbnodejs@gmail.com>
2023-01-30 15:25:07 +0900

    API 조회 추가

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("./schema/post");
+
+router.get('/', async (_, res) => {
+    try {
+        const posts = await Post.find(); // [{}, {}, {}]
+        res.json({ data: posts });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
